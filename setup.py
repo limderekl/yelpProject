499c4162be2faee20913fd0801a17d57604e6dc6
limderekl <limderekl@gmail.com>
2014-11-13 11:10:17 -0800

    added baselines

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def readBusiness(file = "smallBusiness.json"):
         businessDict[newData["business_id"]] = newData
     json_file.close()
     return businessDict
-#outer dict indexed by "user_id,business_id"
+#returns a dict of lists with keys of business_id, all reviews on same business in list
 #def readReview(file = dataDir+"yelp_academic_dataset_review.json"): 
 def readReview(file = "smallReview.json"):
     reviewDict = {}
@@ -76,6 +76,7 @@ def readTip(file = dataDir+"yelp_academic_dataset_tip.json"):
         tipList.append(data)
     json_file.close()
     return tipList
+
 #get num users with review count between min and max, inclusive
 def getNumUserRev(userList, minN, maxN): 
     numUsers = 0
@@ -85,90 +86,28 @@ def getNumUserRev(userList, minN, maxN):
     return numUsers
 
 #returns a random review for a userID
-def getRandUserRev(userID, reviewList): 
+def getRandUserRev(userID, reviewDict): 
     #list of reviews given by userID
     userRevList = []
-    for review in reviewList: 
-        if review["user_id"] == userID: 
-            userRevList.append(review)
+    for allBusRev in reviewDict.values():#allBusRev is a list of all reviews for a given business
+        if allBusRev[0]["user_id"] == userID: 
+            for review in allBusRev:
+                userRevList.append(review)
     #print "user review list : {0}".format(userRevList)
     randRev = random.choice(userRevList)
     #print "rand review : {0}".format(randRev)
     return randRev
     
-#first baseline of prediction based solely on most commonly given rating by user. 
-#baseline1 is a single run, runBaseline1 is multiple
-def baseline1(userID, userList, reviewList): 
-    #get a random user review to be tested on,
-    testRev = getRandUserRev(userID, reviewList)
-    testRevRating= testRev["stars"]
-    #gets avg star rating for userID
-    for user in userList: 
-        if userID == user["user_id"]: 
-            predRating = round(user["average_stars"])
-            break
-    accuracy = 1 if testRevRating == predRating else 0
-    return predRating, accuracy
-
-def runBaseline1(userList, reviewList, revCountThreshold): 
-    totalCount = 0.0
-    numCorrect = 0.0
-    for i in xrange(1000):    
-        #only run for those with 5 or more reviews
-        if userList[i]["review_count"] >=revCountThreshold: 
-            #print "user: {0}".format(userList[i])
-            predRating, accuracy = baseline1(userList[i]["user_id"], userList, reviewList)
-            numCorrect += accuracy #accuracy is 0 for incorrect, 1 for correct
-            totalCount += 1
-    print "totalCount: {0}, numCorrect: {1}, %: {2}".format(totalCount, numCorrect, numCorrect/totalCount)
-    return
-    
-#second baseline of prediction based solely on avg business review
-#baseline2 is a single run, runBaseline2 is multiple
-def baseline2(userID, userList, reviewList, businessList): 
-    #get a random user review to be tested on, returns a tuple
-    testRev = getRandUserRev(userID, reviewList)
-    testBusinessID= testRev["business_id"]
-    testRevRating= testRev["stars"]
-    #gets avg star rating for userID
-    for business in businessList: 
-        if testBusinessID == business["business_id"]:
-            predRating = round(business["stars"])
-            break
-    accuracy = 1 if testRevRating == predRating else 0
-    return predRating, accuracy
-def runBaseline2(userList, reviewList, businessList, revCountThreshold): 
-    totalCount = 0.0
-    numCorrect = 0.0
-    for i in xrange(1000):    
-        #only run for those with review count > threshold
-        if userList[i]["review_count"] >=revCountThreshold: 
-            #print "user: {0}".format(userList[i])
-            predRating, accuracy = baseline2(userList[i]["user_id"], userList, reviewList, businessList)
-            numCorrect += accuracy #accuracy is 0 for incorrect, 1 for correct
-            totalCount += 1
-    print "totalCount: {0}, numCorrect: {1}, %: {2}".format(totalCount, numCorrect, numCorrect/totalCount)
 #returns a dictionary of star rating to count
-def ratingDist(reviewList): 
+def ratingDist(reviewDict): 
     distr = {}
-    for review in reviewList: 
+    for review in reviewDict: 
         distr[review["stars"]] = distr.get(review["stars"],0) + 1
     return distr
-
-def getBusinessRating(businessID, businessList): 
-    for business in businessList: 
-        if businessID == business["business_id"]: 
-            return business["stars"]
-    return 3
 # userDict = readUser()
 # reviewDict = readReview()
 # businessDict = readBusiness()
-# print "userDict : {0}".format(userDict)
-# print ""
-# print "reviewDict: {0}".format(reviewDict)
-# print ""
-# print "businessDict: {0}".format(businessDict)
-
-#runBaseline2(userList, reviewList, businessList, 100)
-#runBaseline1(userList, reviewList, 0)
+# print "userDict : {0}".format(userDict) + '\n'
+# print "reviewDict: {0}".format(reviewDict) + '\n'
+# print "businessDict: {0}".format(businessDict) + '\n'
 #print getNumUserRev(userList, 100,float("inf"))
